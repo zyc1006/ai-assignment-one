0e819fc8a33fe6bfb14ae3f70bf04910bcc86c32
chronicslacker@live.se <chronicslacker@live.se@2354b82b-3731-0dd9-d88b-56c0e8696dd8>
2012-10-03 17:43:13 +0000

    Cleaned up the HFunction class and fixed a casting error.

--- trunk/ ai-assignment-one --username zyc1006@gmail.com/AIAssignment1/Heuristics/HFunction.cs
+++ trunk/ ai-assignment-one --username zyc1006@gmail.com/AIAssignment1/Heuristics/HFunction.cs
@@ -7,51 +7,30 @@ using System.Collections;
 
 namespace AIAssignment1
 {
-    class HFunction    {
-        
+    class HFunction
+    {
+
         public Int32 HFunction(CStateInfo cStateInfo)
         {
-        
-         //Calculate number of actions
- //Servicing Plonks
- int NSP = (1000-cStateInfo.IServicedPlonks)/5;
- //Finding Plonks
- int NFP = (1000- cStateInfo.IServicedPlonks - cStateInfo.IUnservicedPlonks )/10;
- //Servicing Bligs
- int Bh =  8*cStateInfo.ListBligStatus[0]+7* cStateInfo.ListBligStatus[1]+6* cStateInfo.ListBligStatus[2]+5* cStateInfo.ListBligStatus[3]+4* cStateInfo.ListBligStatus[4]+3* cStateInfo.ListBligStatus[5]+2* cStateInfo.ListBligStatus[6]+ cStateInfo.ListBligStatus[7];
- int NSB = (NSP-Bh)/8;
- //Making Spunks
- int NMS = (20*NSP+10*NFP+20*NSB- cStateInfo.ISpunks )/50;
 
- //Calculate TW-hours
- int TWh = (NMS + 2*NSB+NSP)/ cStateInfo.IWorkbenchesAvailable ;
- //and Plink-hours
- int EPh = (NSP+NFP)/ cStateInfo.IPlinksAvailable ;
-  if(EPh > TWh){return EPh;}
-  return TWh;
- }
-       
-       
-       
+            //Calculate number of actions
+            //Servicing Plonks
+            int NSP = (1000 - cStateInfo.IServicedPlonks) / 5;
+            //Finding Plonks
+            int NFP = (1000 - cStateInfo.IServicedPlonks - cStateInfo.IUnservicedPlonks) / 10;
+            //Servicing Bligs
+            int Bh = 8 * (Int32)cStateInfo.ListBligStatus[0] + 7 * (Int32)cStateInfo.ListBligStatus[1] + 6 * (Int32)cStateInfo.ListBligStatus[2] + 5 * (Int32)cStateInfo.ListBligStatus[3] + 4 * (Int32)cStateInfo.ListBligStatus[4] + 3 * (Int32)cStateInfo.ListBligStatus[5] + 2 * (Int32)cStateInfo.ListBligStatus[6] + (Int32)cStateInfo.ListBligStatus[7];
+            int NSB = (NSP - Bh) / 8;
+            //Making Spunks
+            int NMS = (20 * NSP + 10 * NFP + 20 * NSB - cStateInfo.ISpunks) / 50;
+
+            //Calculate TW-hours
+            int TWh = (NMS + 2 * NSB + NSP) / cStateInfo.IWorkbenchesAvailable;
+            //and Plink-hours
+            int EPh = (NSP + NFP) / cStateInfo.IPlinksAvailable;
+            if (EPh > TWh) { return EPh; }
+            return TWh;
+        }
+
     }
 }
-
-
-
-Hide details 
-
-Change log
-r10 by zyc1006@gmail.com on Yesterday (25 hours ago)   Diff 
-1.Bugs fixed on BligeesGo to: 	 
-
-Double click a line to add a comment
-
-Older revisions
- r9 by zyc1006@gmail.com on Yesterday (27 hours ago)   Diff 
- r8 by zyc1006@gmail.com on Sep 30 (2 days ago)   Diff 
- r5 by zyc1006@gmail.com on Sep 28 (4 days ago)   Diff 
-All revisions of this file 
-
-File info
-Size: 11496 bytes, 230 lines
-View raw file
