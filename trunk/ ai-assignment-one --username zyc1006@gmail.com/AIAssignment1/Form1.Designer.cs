1051f32cb7670de396a6540965c90b0f4ff7c9c7
chronicslacker@live.se <chronicslacker@live.se@2354b82b-3731-0dd9-d88b-56c0e8696dd8>
2012-10-03 02:06:08 +0000

    - Added an IWalker interface and used it to define the behavior of CActions. - Also changed the behavior of Form1 so that the the status and actions displays are synchronized. Also the blig display now updates properly when you select a row in the actions display.

--- trunk/ ai-assignment-one --username zyc1006@gmail.com/AIAssignment1/Form1.Designer.cs
+++ trunk/ ai-assignment-one --username zyc1006@gmail.com/AIAssignment1/Form1.Designer.cs
@@ -65,14 +65,14 @@
             this.btStep = new System.Windows.Forms.Button();
             this.btRun = new System.Windows.Forms.Button();
             this.dgvAction = new System.Windows.Forms.DataGridView();
-            this.btSearch = new System.Windows.Forms.Button();
-            this.textBox1 = new System.Windows.Forms.TextBox();
             this.claTime = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.clMS = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.clSBN = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.clSBF = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.clFP = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.clSP = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.btSearch = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
             this.groupBox1.SuspendLayout();
             this.groupBox2.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dgvInfo)).BeginInit();
@@ -83,9 +83,9 @@
             // 
             // btStart
             // 
-            this.btStart.Location = new System.Drawing.Point(229, 133);
+            this.btStart.Location = new System.Drawing.Point(229, 144);
             this.btStart.Name = "btStart";
-            this.btStart.Size = new System.Drawing.Size(67, 23);
+            this.btStart.Size = new System.Drawing.Size(67, 25);
             this.btStart.TabIndex = 8;
             this.btStart.Text = "START";
             this.btStart.UseVisualStyleBackColor = true;
@@ -94,27 +94,27 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(23, 32);
+            this.label1.Location = new System.Drawing.Point(23, 35);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(59, 12);
+            this.label1.Size = new System.Drawing.Size(58, 13);
             this.label1.TabIndex = 1;
             this.label1.Text = "Spunkees:";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(25, 59);
+            this.label2.Location = new System.Drawing.Point(25, 64);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(53, 12);
+            this.label2.Size = new System.Drawing.Size(44, 13);
             this.label2.TabIndex = 2;
             this.label2.Text = "Bligees:";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(25, 87);
+            this.label3.Location = new System.Drawing.Point(25, 94);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(47, 12);
+            this.label3.Size = new System.Drawing.Size(38, 13);
             this.label3.TabIndex = 3;
             this.label3.Text = "Plinks:";
             // 
@@ -126,36 +126,36 @@
             this.groupBox1.Controls.Add(this.label1);
             this.groupBox1.Controls.Add(this.label3);
             this.groupBox1.Controls.Add(this.label2);
-            this.groupBox1.Location = new System.Drawing.Point(31, 23);
+            this.groupBox1.Location = new System.Drawing.Point(31, 25);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(166, 133);
+            this.groupBox1.Size = new System.Drawing.Size(166, 144);
             this.groupBox1.TabIndex = 4;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Employees";
             // 
             // txTotalPl
             // 
-            this.txTotalPl.Location = new System.Drawing.Point(99, 84);
+            this.txTotalPl.Location = new System.Drawing.Point(99, 91);
             this.txTotalPl.Name = "txTotalPl";
-            this.txTotalPl.Size = new System.Drawing.Size(33, 21);
+            this.txTotalPl.Size = new System.Drawing.Size(33, 20);
             this.txTotalPl.TabIndex = 6;
             this.txTotalPl.Text = "10";
             this.txTotalPl.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
             // 
             // txTotalBlige
             // 
-            this.txTotalBlige.Location = new System.Drawing.Point(99, 56);
+            this.txTotalBlige.Location = new System.Drawing.Point(99, 61);
             this.txTotalBlige.Name = "txTotalBlige";
-            this.txTotalBlige.Size = new System.Drawing.Size(33, 21);
+            this.txTotalBlige.Size = new System.Drawing.Size(33, 20);
             this.txTotalBlige.TabIndex = 5;
             this.txTotalBlige.Text = "10";
             this.txTotalBlige.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
             // 
             // txTotalSp
             // 
-            this.txTotalSp.Location = new System.Drawing.Point(99, 29);
+            this.txTotalSp.Location = new System.Drawing.Point(99, 31);
             this.txTotalSp.Name = "txTotalSp";
-            this.txTotalSp.Size = new System.Drawing.Size(33, 21);
+            this.txTotalSp.Size = new System.Drawing.Size(33, 20);
             this.txTotalSp.TabIndex = 4;
             this.txTotalSp.Text = "10";
             this.txTotalSp.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
@@ -166,27 +166,27 @@
             this.groupBox2.Controls.Add(this.txTotalBlig);
             this.groupBox2.Controls.Add(this.label4);
             this.groupBox2.Controls.Add(this.label6);
-            this.groupBox2.Location = new System.Drawing.Point(229, 23);
+            this.groupBox2.Location = new System.Drawing.Point(229, 25);
             this.groupBox2.Name = "groupBox2";
-            this.groupBox2.Size = new System.Drawing.Size(160, 99);
+            this.groupBox2.Size = new System.Drawing.Size(160, 107);
             this.groupBox2.TabIndex = 5;
             this.groupBox2.TabStop = false;
             this.groupBox2.Text = "Tools";
             // 
             // txTotalWb
             // 
-            this.txTotalWb.Location = new System.Drawing.Point(100, 56);
+            this.txTotalWb.Location = new System.Drawing.Point(100, 61);
             this.txTotalWb.Name = "txTotalWb";
-            this.txTotalWb.Size = new System.Drawing.Size(33, 21);
+            this.txTotalWb.Size = new System.Drawing.Size(33, 20);
             this.txTotalWb.TabIndex = 7;
             this.txTotalWb.Text = "10";
             this.txTotalWb.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
             // 
             // txTotalBlig
             // 
-            this.txTotalBlig.Location = new System.Drawing.Point(100, 29);
+            this.txTotalBlig.Location = new System.Drawing.Point(100, 31);
             this.txTotalBlig.Name = "txTotalBlig";
-            this.txTotalBlig.Size = new System.Drawing.Size(33, 21);
+            this.txTotalBlig.Size = new System.Drawing.Size(33, 20);
             this.txTotalBlig.TabIndex = 6;
             this.txTotalBlig.Text = "10";
             this.txTotalBlig.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
@@ -194,18 +194,18 @@
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(16, 32);
+            this.label4.Location = new System.Drawing.Point(16, 35);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(41, 12);
+            this.label4.Size = new System.Drawing.Size(32, 13);
             this.label4.TabIndex = 4;
             this.label4.Text = "Bligs:";
             // 
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(18, 59);
+            this.label6.Location = new System.Drawing.Point(18, 64);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(77, 12);
+            this.label6.Size = new System.Drawing.Size(77, 13);
             this.label6.TabIndex = 5;
             this.label6.Text = "Workbenches:";
             // 
@@ -234,7 +234,7 @@
             this.clSpunks,
             this.clUnPlonks,
             this.clSePlonks});
-            this.dgvInfo.Location = new System.Drawing.Point(31, 178);
+            this.dgvInfo.Location = new System.Drawing.Point(31, 193);
             this.dgvInfo.MultiSelect = false;
             this.dgvInfo.Name = "dgvInfo";
             this.dgvInfo.ReadOnly = true;
@@ -249,9 +249,9 @@
             this.dgvInfo.RowHeadersVisible = false;
             this.dgvInfo.RowHeadersWidth = 36;
             this.dgvInfo.RowTemplate.Height = 23;
-            this.dgvInfo.Size = new System.Drawing.Size(449, 199);
+            this.dgvInfo.Size = new System.Drawing.Size(449, 216);
             this.dgvInfo.TabIndex = 6;
-            this.dgvInfo.CellMouseClick += new System.Windows.Forms.DataGridViewCellMouseEventHandler(this.dgvInfo_CellMouseClick);
+            this.dgvInfo.SelectionChanged += new System.EventHandler(this.dgvInfo_SelectionChanged);
             // 
             // clTime
             // 
@@ -324,7 +324,7 @@
             this.clBligs,
             this.clBligeesNormal,
             this.clBligeesFast});
-            this.dgvBlInfo.Location = new System.Drawing.Point(510, 178);
+            this.dgvBlInfo.Location = new System.Drawing.Point(510, 193);
             this.dgvBlInfo.MultiSelect = false;
             this.dgvBlInfo.Name = "dgvBlInfo";
             this.dgvBlInfo.ReadOnly = true;
@@ -339,7 +339,7 @@
             this.dgvBlInfo.RowHeadersVisible = false;
             this.dgvBlInfo.RowHeadersWidth = 22;
             this.dgvBlInfo.RowTemplate.Height = 23;
-            this.dgvBlInfo.Size = new System.Drawing.Size(262, 242);
+            this.dgvBlInfo.Size = new System.Drawing.Size(262, 262);
             this.dgvBlInfo.TabIndex = 9;
             // 
             // clHour
@@ -373,17 +373,17 @@
             // lbMessage
             // 
             this.lbMessage.AutoSize = true;
-            this.lbMessage.Location = new System.Drawing.Point(18, 29);
+            this.lbMessage.Location = new System.Drawing.Point(18, 31);
             this.lbMessage.Name = "lbMessage";
-            this.lbMessage.Size = new System.Drawing.Size(0, 12);
+            this.lbMessage.Size = new System.Drawing.Size(0, 13);
             this.lbMessage.TabIndex = 10;
             // 
             // groupBox3
             // 
             this.groupBox3.Controls.Add(this.lbMessage);
-            this.groupBox3.Location = new System.Drawing.Point(412, 22);
+            this.groupBox3.Location = new System.Drawing.Point(412, 24);
             this.groupBox3.Name = "groupBox3";
-            this.groupBox3.Size = new System.Drawing.Size(140, 100);
+            this.groupBox3.Size = new System.Drawing.Size(140, 108);
             this.groupBox3.TabIndex = 11;
             this.groupBox3.TabStop = false;
             this.groupBox3.Text = "Message";
@@ -391,9 +391,9 @@
             // btStep
             // 
             this.btStep.Enabled = false;
-            this.btStep.Location = new System.Drawing.Point(311, 133);
+            this.btStep.Location = new System.Drawing.Point(311, 144);
             this.btStep.Name = "btStep";
-            this.btStep.Size = new System.Drawing.Size(66, 23);
+            this.btStep.Size = new System.Drawing.Size(66, 25);
             this.btStep.TabIndex = 9;
             this.btStep.Text = "STEP";
             this.btStep.UseVisualStyleBackColor = true;
@@ -402,9 +402,9 @@
             // btRun
             // 
             this.btRun.Enabled = false;
-            this.btRun.Location = new System.Drawing.Point(396, 133);
+            this.btRun.Location = new System.Drawing.Point(396, 144);
             this.btRun.Name = "btRun";
-            this.btRun.Size = new System.Drawing.Size(63, 23);
+            this.btRun.Size = new System.Drawing.Size(63, 25);
             this.btRun.TabIndex = 12;
             this.btRun.Text = "RUN";
             this.btRun.UseVisualStyleBackColor = true;
@@ -434,7 +434,7 @@
             this.clSBF,
             this.clFP,
             this.clSP});
-            this.dgvAction.Location = new System.Drawing.Point(571, 23);
+            this.dgvAction.Location = new System.Drawing.Point(571, 25);
             this.dgvAction.MultiSelect = false;
             this.dgvAction.Name = "dgvAction";
             this.dgvAction.ReadOnly = true;
@@ -449,24 +449,9 @@
             this.dgvAction.RowHeadersVisible = false;
             this.dgvAction.RowHeadersWidth = 36;
             this.dgvAction.RowTemplate.Height = 23;
-            this.dgvAction.Size = new System.Drawing.Size(197, 133);
+            this.dgvAction.Size = new System.Drawing.Size(197, 144);
             this.dgvAction.TabIndex = 13;
-            // 
-            // btSearch
-            // 
-            this.btSearch.Location = new System.Drawing.Point(50, 396);
-            this.btSearch.Name = "btSearch";
-            this.btSearch.Size = new System.Drawing.Size(75, 23);
-            this.btSearch.TabIndex = 14;
-            this.btSearch.Text = "SEARCH";
-            this.btSearch.UseVisualStyleBackColor = true;
-            // 
-            // textBox1
-            // 
-            this.textBox1.Location = new System.Drawing.Point(174, 398);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 21);
-            this.textBox1.TabIndex = 15;
+            this.dgvAction.SelectionChanged += new System.EventHandler(this.dgvAction_SelectionChanged);
             // 
             // claTime
             // 
@@ -516,11 +501,27 @@
             this.clSP.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
             this.clSP.Width = 23;
             // 
+            // btSearch
+            // 
+            this.btSearch.Location = new System.Drawing.Point(50, 429);
+            this.btSearch.Name = "btSearch";
+            this.btSearch.Size = new System.Drawing.Size(75, 25);
+            this.btSearch.TabIndex = 14;
+            this.btSearch.Text = "SEARCH";
+            this.btSearch.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(174, 431);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 15;
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(807, 444);
+            this.ClientSize = new System.Drawing.Size(807, 481);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.btSearch);
             this.Controls.Add(this.btRun);
