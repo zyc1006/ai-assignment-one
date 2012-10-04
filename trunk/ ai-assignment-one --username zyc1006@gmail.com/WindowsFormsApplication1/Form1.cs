fdb0481ac9505ee14bff6363ba27e117ace7e3a5
chronicslacker@live.se <chronicslacker@live.se@2354b82b-3731-0dd9-d88b-56c0e8696dd8>
2012-10-04 04:57:50 +0000

    Added a new suggestion-calculating algorithm in a separate project. The algorithm uses a graph representation with our adjustable resources as nodes.
    The new project implements the algorithm in a new Form that launches along with the main Form.
    
    When testing it has consistently given suggestions that improve the plan, converging on 29 hours. If we try to run the algorithm on the excess of a 29h long plan, the generated suggestion will result in a plan that's roughly 36h long, and will then start converging on 29h again.
    
    -[The algorithm]---------------------------------
    
    Each node is annotated with the average amount of excess units to redistributed. Additionally each node is annotated with the suggested adjustment to that resource type (initialized to 0).
    
    Any two resources that appear together in the precondition of at least 1 action are connected in the graph. Each connection has 2 values: the amount that can be through that link (called "from" value) and the resulting amount of adjustment to the receiving node (called the "to" value).
    
    Excess resources can be moved from a given node if, and only if, there is enough excess left in the node to distribute uniformly across all connections to that node. This will result in a negative change to the adjustment value of the node, and a positive change to the adjustment value of it's neighbors.
    
    When a the adjustment value of a node is increased, the excess value of the neighboring nodes should be decreased by a corresponding amount. This reflects that we can now potentially perform more actions using the previously unused resources.
    
    The algorithm halts when we can no longer move any excess resources.

---
+++ trunk/ ai-assignment-one --username zyc1006@gmail.com/WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,206 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class AdjustmentCaculator : Form
+    {
+
+        private int excessESp;
+        private int excessEB;
+        private int excessEP;
+        private int excessTW;
+        private int excessTB;
+
+        private Calculator calc = new Calculator();
+
+        public AdjustmentCaculator()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            MessageBox.Text = "";
+            if (!tryGetExcessVals())
+                return;
+
+            calc.calculate(excessESp, excessEB, excessEP, excessTW, excessTB);
+
+            tbESpAdjustment.Text = calc.adjESp.ToString();
+            tbEBAdjustment.Text = calc.adjEB.ToString();
+            tbEPAdjustment.Text = calc.adjEP.ToString();
+            tbTWAdjustment.Text = calc.adjTW.ToString();
+            tbTBAdjustment.Text = calc.adjTB.ToString();
+        }
+
+        private bool tryGetExcessVals()
+        {
+            bool r = true;
+
+            try{
+                excessESp = Convert.ToInt32(tbESpExcess.Text);
+            }
+            catch (Exception){
+                MessageBox.Text += "\n\rInvalid input for excess ESp.";
+                tbESpExcess.Select();
+                r = false;
+            }
+            try{
+                excessEB = Convert.ToInt32(tbEBExcess.Text);
+            }
+            catch (Exception){
+                MessageBox.Text += "\n\rInvalid input for excess ESp.";
+                tbEBExcess.Select();
+                r = false;
+            }
+            try{
+                excessEP = Convert.ToInt32(tbEPExcess.Text);
+            }
+            catch (Exception){
+                MessageBox.Text += "\nInvalid input for excess ESp.";
+                tbEPExcess.Select();
+                r = false;
+            }
+            try{
+                excessTW = Convert.ToInt32(tbTWExcess.Text);
+            }
+            catch (Exception){
+                MessageBox.Text += "\nInvalid input for excess ESp.";
+                tbTWExcess.Select();
+                r = false;
+            }
+            try
+            {
+                excessTB = Convert.ToInt32(tbTBExcess.Text);
+            }
+            catch (Exception)
+            {
+                MessageBox.Text += "\nInvalid input for excess ESp.";
+                tbTBExcess.Select();
+                r = false;
+            }
+
+            return r;
+        }
+
+        private class Calculator
+        {
+
+            public int adjESp { get; private set; }
+            public int adjEB { get; private set; }
+            public int adjEP { get; private set; }
+            public int adjTW { get; private set; }
+            public int adjTB { get; private set; }
+
+            public void calculate(int excessESp, int excessEB, int excessEP, int excessTW, int excessTB)
+            {
+                ExcessNode ESp = new ExcessNode(excessESp);
+                ExcessNode EB = new ExcessNode(excessEB);
+                ExcessNode EP = new ExcessNode(excessEP);
+                ExcessNode TW = new ExcessNode(excessTW);
+                ExcessNode TB = new ExcessNode(excessTB);
+
+                ESp.addLink(TW, 1, 1);
+
+                TW.addLink(ESp, 1, 1);
+                TW.addLink(EB, 1, 1);
+                TW.addLink(EP, 1, 1);
+                TW.addLink(TB, 1, 1);
+
+                TB.addLink(TW, 1, 1);
+                TB.addLink(EB, 1, 2);
+                TB.addLink(EP, 1, 1);
+
+                EB.addLink(TB, 2, 1);
+                EB.addLink(TW, 1, 1);
+
+                EP.addLink(TW, 1, 1);
+                EP.addLink(TB, 1, 1);
+
+                Queue<ExcessNode> nodes = new Queue<ExcessNode>();
+                nodes.Enqueue(ESp);
+                nodes.Enqueue(EB);
+                nodes.Enqueue(EP);
+                nodes.Enqueue(TW);
+                nodes.Enqueue(TB);
+
+                while (nodes.Count > 0){
+                    ExcessNode n = nodes.Dequeue();
+                    if (n.tryPushExcess())
+                        nodes.Enqueue(n);
+                }
+
+                adjESp = ESp.Adjustment;
+                adjEB = EB.Adjustment;
+                adjEP = EP.Adjustment;
+                adjTW = TW.Adjustment;
+                adjTB = TB.Adjustment;
+            }
+        }
+
+        private class ExcessNode
+        {
+            private int excess = 0;
+            private int adjustment = 0;
+            private int fromWeightsTot = 0;
+            private Dictionary<ExcessNode, ExchangeRate> transitions = new Dictionary<ExcessNode, ExchangeRate>();
+
+            public int Excess { get { return excess; }}
+            public int Adjustment { get { return adjustment; }}
+
+            public ExcessNode(int excess)
+            {
+                this.excess = excess;
+            }
+
+            public bool tryPushExcess()
+            {
+                if (excess < fromWeightsTot)
+                    return false;
+
+                foreach(KeyValuePair<ExcessNode, ExchangeRate> t in transitions ){
+                    adjustment -= t.Value.From;
+                    t.Key.adjust(t.Value.To);
+                }
+                
+                return true;
+            }
+
+            public void adjust(int adjustment)
+            {
+                this.adjustment += adjustment;
+
+                foreach(KeyValuePair<ExcessNode, ExchangeRate> t in transitions){
+                    t.Key.decrease(adjustment);
+                }
+            }
+
+            public void decrease(int amount)
+            {
+                excess -= amount;
+            }
+
+            public void addLink(ExcessNode n, int from, int to)
+            {
+                ExchangeRate er = new ExchangeRate();
+                er.From = from;
+                er.To = to;
+                transitions.Add(n, er);
+                fromWeightsTot += from;
+            }
+        }
+
+        private class ExchangeRate
+        {
+            public int From = 1;
+            public int To = 1;
+        }
+    }
+}
