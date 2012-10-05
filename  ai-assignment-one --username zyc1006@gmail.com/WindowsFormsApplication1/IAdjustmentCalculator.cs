e9e7868347e5079bff787641e4cef13d586a99c0
chronicslacker@live.se <chronicslacker@live.se@2354b82b-3731-0dd9-d88b-56c0e8696dd8>
2012-10-05 13:29:36 +0000

    Generalized the "adjustment calculator" concept by adding the IAdjustmentCalculator interface that can be implemented by any class acting as an adjustment Calculator.

---
+++  ai-assignment-one --username zyc1006@gmail.com/WindowsFormsApplication1/IAdjustmentCalculator.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WindowsFormsApplication1
+{
+    interface IAdjustmentCalculator
+    {
+        int AdjESp { get; }
+        int AdjEB { get; }
+        int AdjEP { get; }
+        int AdjTW { get; }
+        int AdjTB { get; }
+
+        void calculate(int excessESp, int excessEB, int excessEP, int excessTW, int excessTB);
+    }
+}
